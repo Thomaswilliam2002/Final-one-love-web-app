3ad75ac9239bed23da18ed48cf38cef1921aa884
William Hounwanou <williamhounwanou2002@email.com>
2026-06-08 13:31:20 +0100

    x9

--- src/models/histCaise.js
+++ src/models/histCaise.js
@@ -33,7 +33,6 @@ module.exports = (sequelize, DataTypes) => {
         date: {
             type: DataTypes.DATEONLY,
             allowNull: false,
-            defaultValue: sequelize.literal('CURRENT_DATE')
         },
         is_active: {
             type: DataTypes.BOOLEAN,

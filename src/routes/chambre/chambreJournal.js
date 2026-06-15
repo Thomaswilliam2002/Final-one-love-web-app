46b8c006526043e5acfdf4355a2d27978a76ef42
William Hounwanou <williamhounwanou2002@email.com>
2026-06-15 17:16:00 +0100

    modif de la route chambreJournal pour plus de precision au niveau du message de succes de l'ajout de journal d'une chambre apres payement x2

--- src/routes/chambre/chambreJournal.js
+++ src/routes/chambre/chambreJournal.js
@@ -52,7 +52,7 @@ addChJournal = (app) => {
             manquant: manquant,
         })
             .then(chambreJournal => {
-                res.redirect('/formFondBarClub?msg=Journal ajouter avec succes pour la chambre ' + chambreNom + ' et la maison colse ' + mclose1 + '&type=mclose')
+                res.redirect('/formFondBarClub?msg=Journal ajouter avec succes pour la chambre ' + chambreNom + ' de la maison colse ' + mclose1 + '&type=mclose')
             })
             .catch(_ => {
                 console.error(_);
